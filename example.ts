14691d4954dd762b3fd8bbb6de3898c1b1da3052
Vova <herogaming@yandex.ua>
2018-07-17 16:59:50 +0300

    load ready files

---
+++ example.ts
@@ -0,0 +1,12 @@
+/// <reference path="./src/GCollection.ts" />
+/// <reference path="./src/GCollectable.ts" />
+
+const coll: GCollection = new GCollection();
+
+for(let i = 1; i < 10; i++){
+    coll.add(<GCollectable>{id:0, parrent: undefined, numb: i})
+}
+
+coll.each((child: any, id: number)=>{
+    console.log(child.numb, id);
+});
